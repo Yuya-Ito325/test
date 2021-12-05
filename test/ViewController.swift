c5a8cb2b31115ce4fc885727c98f246a82d2ae49
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2021-12-05 18:05:35 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 伊藤祐哉 on 2021/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
